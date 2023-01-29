102aa728813b91edf1463d8c4bea1a99ef2a43a4
Maciej Rybak <maciek.rybak95@gmail.com>
2023-01-29 11:24:42 +0100

    React hook useReducer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useReducer, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const samurajProgramowaniaCourses = [
+  {
+    id: "aa2561a3-861a-4d3b-bf32-deeca65c1dab",
+    title: "Web developer od podstaw w 15 dni",
+  },
+  {
+    id: "d257b2cf-5200-46d4-8889-d7d9cd0cfa86",
+    title: "Zaawansowany front-end w 15 dni",
+  },
+  {
+    id: "7bd99654-d51d-4d62-ad66-353866d4bd01",
+    title: "Programowanie w JavaScript",
+  },
+  {
+    id: "3cae4405-bc0d-459e-abd7-3fcef7fcc3a5",
+    title: "React od podstaw - teoria i praktyka",
+  },
+  {
+    id: "f8c28740-0184-423c-ad03-018496946179",
+    title: "Backend - Node.js, Express i MongoDB",
+  },
+  {
+    id: "20557edc-c01e-48a4-814f-61c9c7f0850a",
+    title: "(Zaawansowane) Projekty w CSS i JavaScript",
+  },
+];
+
+const App = () => {
+  return <div>TEst</div>;
+};
 
 export default App;
